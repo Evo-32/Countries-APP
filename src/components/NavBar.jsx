dd98d7a51452d1da689829d13459b0a2448dfa12
Evo-32 <vodospearl@gmail.com>
2024-04-01 17:52:12 +0200

    count app

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import MenuIcon from "./MenuIcon";
+import LogoIcon from "./LogoIcon";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="navbar flex justify-between items-center text-slate-50 px-40 py-10">
+      <ul className="flex items-center">
+        <li>
+          <a href="#">
+            <LogoIcon />
+          </a>
+        </li>
+      </ul>
+      <div className="flex items-center">
+        <ul className="nav-link space-x-4 hidden md:flex">
+          <li>
+            <a href="#">Countries</a>
+          </li>
+          <li>
+            <a href="#">Continents</a>
+          </li>
+          <li>
+            <a href="#">Region</a>
+          </li>
+        </ul>
+        <button className="md:hidden">
+          <MenuIcon className="text-white text-2xl font-bold" />
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
